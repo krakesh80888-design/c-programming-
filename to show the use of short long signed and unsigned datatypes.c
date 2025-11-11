fc4634815ddc90a549448ff5f611db2c3a47c6eb
krakesh80888-design <krakesh80888@gmail.com>
2025-11-11 08:50:40 +0530

    Add files via upload

---
+++ to show the use of short long signed and unsigned datatypes.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+short int a=32767;
+unsigned int b=65535;
+long int c=123456789;
+signed int d=-120;
+printf("short int:%d\n",a);
+printf("unsigned int:%u\n",b);
+printf("long int:%ld\n",c);
+printf("signed int:%d\n",d);
+return 0;
+}
